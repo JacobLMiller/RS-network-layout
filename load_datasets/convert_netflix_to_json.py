06551ae1f9575fdbd27776b7f9dd83530468ee79
Kaviru Gunaratne <go69bes@mytum.de>
2025-08-03 21:26:33 +0200

    Optimize scraping of movie descriptions and allow option to remove sparse nodes without a minimum degree.

--- load_datasets/convert_netflix_to_json.py
+++ load_datasets/convert_netflix_to_json.py
@@ -32,18 +32,17 @@ def save_movie_plot(raw_data_dir="raw_data/netflix", write_file_name="movie_info
     with open(os.path.join(raw_data_dir, movie_titles_file), encoding="ISO-8859-1") as f:
         reader = csv.reader(f)
         for row in reader:
-            print(row)
             movie_id, year, = row[:2]
             titles = row[2:]
-            titles = [title for t in titles for title in t.split('/')] # TODO Some titles contain commas (e.g. 20,000 Leagues Under the Sea) so CSV reader splits the title
-
-            movie_info[movie_id] = {"year": year, "titles": list(titles)}
+            # titles = [title for t in titles for title in t.split('/')]
+            title = ", ".join(titles) # Because maybe commas were part of the movie - but if they are actually two different titles, hopefully still won't be a problem? ¯\_(ツ)_/¯
+            movie_info[movie_id] = {"year": year, "title": [title]}
 
     ia = Cinemagoer()
 
     with tqdm(movie_info) as pbar:
         for movie_id in pbar:
-            titles = movie_info[movie_id]
+            titles = movie_info[movie_id]['title']
             pbar.set_postfix(id=movie_id, titles=titles)
 
             if not titles:
@@ -68,9 +67,10 @@ def save_movie_plot(raw_data_dir="raw_data/netflix", write_file_name="movie_info
                                 description = description.split('::')[0].strip()
                         else:
                             description = movie.get('plot outline', '') # Empty if no plot outline
-                        if not description: continue
-
+                        # if not description: continue
+  
                         movie_info[movie_id]['data'] = description
+                        print(movie_id, description[:100] + "...")
 
                         break
 
@@ -82,9 +82,10 @@ def save_movie_plot(raw_data_dir="raw_data/netflix", write_file_name="movie_info
 
     return movie_info
 
-def save_netflix_as_json(movie_info, raw_data_dir, write_dir):
+def save_netflix_as_json(movie_info, raw_data_dir, write_dir, min_sparse_degree=300):
     import json
     import os
+    from collections import defaultdict
 
     combined_files = [
         "combined_data_1.txt",
@@ -95,6 +96,7 @@ def save_netflix_as_json(movie_info, raw_data_dir, write_dir):
 
     for i, filename in enumerate(combined_files):
         movie_data = {}
+        movies_wo_info = []
         with open(os.path.join(raw_data_dir, filename), "r") as f:
             current_movie_id = None
             for line in f:
@@ -105,15 +107,16 @@ def save_netflix_as_json(movie_info, raw_data_dir, write_dir):
                     if current_movie_id in movie_info:
                         movie_data[key] = {
                             "year": movie_info[current_movie_id]["year"],
-                            "titles": movie_info[current_movie_id]["titles"],
+                            "title": movie_info[current_movie_id]["title"],
                             "data": movie_info[current_movie_id]["data"],
                             "s_ids": []
                         }
                     else:
-                        # If movie_id not in movie_titles.csv, skip or handle as needed
+                        # If movie_id not in movie_titles.csv just skip this node
+                        movies_wo_info.append(key)
                         movie_data[key] = {
                             "year": None,
-                            "titles": None,
+                            "title": None,
                             "data": None,
                             "s_ids": []
                         }
@@ -121,11 +124,33 @@ def save_netflix_as_json(movie_info, raw_data_dir, write_dir):
                     person_id = line.split(",")[0]
                     movie_data[f"r_{current_movie_id}"]["s_ids"].append("s_" + person_id)
 
+        # Remove movies without data
+        for movie_id in movies_wo_info:
+            movie_data.pop(movie_id)
+
+
+        # Remove s_ids that have degree lower than min_sparse_degree
+        # And remove movies that get a 0 degree as a result of this
+        s_id_degree = defaultdict(int)
+        for data in movie_data.values():
+            for s_id in data['s_ids']:
+                s_id_degree[s_id] += 1
+
+        free_node_movies = list()
+        for movie_id, data in movie_data.items():
+            data['s_ids'] = [s_id for s_id in data['s_ids'] if s_id_degree[s_id] > min_sparse_degree]
+            if not data['s_ids']:
+                free_node_movies.append(movie_id)
+        for movie_id in free_node_movies:
+            movie_data.pop(movie_id)
+
+
         rich_data: list = [{'id': r_id} | r_data for r_id, r_data in movie_data.items()]
         del movie_data
 
         unique_s_ids = sorted(set(s_id for r_data in rich_data for s_id in r_data['s_ids']),
                             key=lambda x: int(x.split('_')[1]))
+        
         sparse_data: list = [{'id': s_id} for s_id in unique_s_ids]
 
 
@@ -144,5 +169,23 @@ def convert_netflix(target_dir="json_data"):
     raw_data_dir = "raw_data/netflix"
     movie_info_file = "movie_info.json"
     movie_info = save_movie_plot(raw_data_dir, movie_info_file)
+
+    save_netflix_as_json(movie_info, raw_data_dir, target_dir)
+
+if __name__ == "__main__":
+    import os
+    import json
+
+    raw_data_dir = "raw_data/netflix"
+    movie_info_file = "movie_info.json"
+
+    with open(os.path.join(raw_data_dir, movie_info_file), 'r') as f:
+        movie_info = json.load(f)
+
+    movie_info = {id: info for id, info in movie_info.items() if (('data' in info) and (info['data'] not in [None, ""]))}
+
+    target_dir = "json_data"
+    if not os.path.isdir(target_dir):
+        os.mkdir(target_dir)
 
     save_netflix_as_json(movie_info, raw_data_dir, target_dir)
