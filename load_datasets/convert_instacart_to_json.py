239848f38e6f5291cde10461397c5151b1e236d3
Kaviru Gunaratne <go69bes@mytum.de>
2025-08-10 07:48:54 +0200

    Instacart dataset: bugfix: account for multi-edges (by adding edge weights), and add metadata section to JSON file comprising the parameter values used

--- load_datasets/convert_instacart_to_json.py
+++ load_datasets/convert_instacart_to_json.py
@@ -2,9 +2,10 @@ import os
 import json
 import csv
 from collections import defaultdict
+from collections import Counter
 
 
-def convert_instacart(target_dir="json_data", raw_data_dir = "raw_data/instacart"):
+def convert_instacart(target_dir="json_dir", raw_data_dir = "raw_data/instacart", min_rich_deg=150, min_sparse_deg=25):
     """
     Save instacart dataset
     """
@@ -71,37 +72,78 @@ def convert_instacart(target_dir="json_data", raw_data_dir = "raw_data/instacart
         s_id = f"s_{order_data['user_id']}"
         prod_orders[product_id].append(s_id)
 
+    # Weighted edges
+    for prod_id, s_ids in prod_orders.items():
+        prod_orders[prod_id] = [(s_id, weight) for s_id, weight in Counter(s_ids).items()]
 
 
+
+    # Only keep rich nodes with minimum degree min_rich_deg
+    top_prods = ([prod_id for prod_id in prod_orders if len(prod_orders[prod_id]) >= min_rich_deg])
+    prod_orders = {prod_id: prod_orders[prod_id] for prod_id in top_prods}
+
+    # Only keep sparse nodes with minimum degree min_sparse_deg
+    s_id_degree = defaultdict(int)
+    for s_ids_w_wt in prod_orders.values():
+        for s_id, _ in s_ids_w_wt:
+            s_id_degree[s_id] += 1
+
+    print(f"Number of initial s_ids: {len(s_id_degree)}")
+    # print({s_id : degree for s_id, degree in s_id_degree.items() if degree < 2})
+
+    isolated_prods = list()
+    for prod_id in prod_orders:
+        prod_orders[prod_id] = [(s_id, weight) for s_id, weight in prod_orders[prod_id] if s_id_degree[s_id] >= min_sparse_deg]
+        if not prod_orders[prod_id]:
+            isolated_prods.append(prod_id)
+    # Remove rich nodes that are isolated as a result of the previous step
+    for prod_id in isolated_prods:
+        prod_orders.pop(prod_id)
+
+    print("After filtering:")
+    print("----------------")
+    s_id_degree = defaultdict(int)
+    for s_ids in prod_orders.values():
+        for s_id, _ in s_ids:
+            s_id_degree[s_id] += 1
+
+    print(f"Number of final s_ids: {len(s_id_degree)}")
+    # print({s_id : degree for s_id, degree in s_id_degree.items() if degree < 2})
 
     # Rich data
     rich_data = list()
-    for product_id, s_ids in prod_orders.items():
+    for product_id, s_ids_w_wt in prod_orders.items():
         data = list(products[product_id].values())
         rich_data.append(
             {
                 "id" : f"r_{product_id}",
                 "data": data,
-                "s_ids": s_ids
+                "s_ids": s_ids_w_wt
             }
         )
 
     # Sparse data
-    unique_s_ids = sorted(set(s_id for r_data in rich_data for s_id in r_data['s_ids']),
+    unique_s_ids = sorted(set(s_id for r_data in rich_data for s_id, _ in r_data['s_ids']),
                             key=lambda x: int(x.split('_')[1]))
     sparse_data: list = [{'id': s_id} for s_id in unique_s_ids]
 
 
 
     data_dict = {
+        '_processing_comments' : {
+            "Min rich deg": min_rich_deg,
+            "Min sparse deg": min_sparse_deg
+        },
         'rich': rich_data,
         'sparse': sparse_data,
     }
 
-    file_name = "instacart_graph.json"
+    file_name = "instacart.json"
     with open(os.path.join(target_dir, file_name), 'w', encoding='utf-8') as json_file:
         json.dump(data_dict, json_file, ensure_ascii=False, indent=4)
     print(f"Dictionary saved to {file_name}")
+
+
 
 
 if __name__ == "__main__":
